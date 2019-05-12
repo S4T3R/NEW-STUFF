a62f9741059208bc6c65a7d5390caeb87b0c531e
Matin <martin188@gmail.com>
2019-05-12 16:37:19 +0700

    C4T 18

--- Session 1/123.py
+++ Session 1/123.py
@@ -4,7 +4,14 @@
 # string1 = " aloha"
 # print(var + var1)
 # print(string + string1)
+# from math import *
+# Radius = int(input("radius: "))
+# sum = Radius * pi * 2
+# sum1 = Radius ** 2 * pi
+# print (Radius, "*", pi, "*", 2, "=", sum  )
+# print (Radius ** 2, "*", pi , "=", sum1  )
 
-so = int(input("pick a number: "))
-so1 = int(input("pick a number: "))
-print (so + so1)
+Năm_sinh = int(input("nhập năm sinh: "))
+Năm_nay = 2019
+Tuổi = 2019 - Năm_sinh
+print (Năm_nay, "-", Năm_sinh, "=", Tuổi) 
