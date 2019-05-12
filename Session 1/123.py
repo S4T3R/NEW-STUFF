c795f099e5c1ac325d2410c506362acea5ed0808
Matin <martin188@gmail.com>
2019-05-12 16:41:31 +0700

    BTVN,Đỗ Vũ Thành

--- Session 1/123.py
+++ Session 1/123.py
@@ -11,7 +11,7 @@
 # print (Radius, "*", pi, "*", 2, "=", sum  )
 # print (Radius ** 2, "*", pi , "=", sum1  )
 
-Năm_sinh = int(input("nhập năm sinh: "))
-Năm_nay = 2019
-Tuổi = 2019 - Năm_sinh
-print (Năm_nay, "-", Năm_sinh, "=", Tuổi) 
+# Năm_sinh = int(input("nhập năm sinh: "))
+# Năm_nay = 2019
+# Tuổi = 2019 - Năm_sinh
+# print (Năm_nay, "-", Năm_sinh, "=", Tuổi) 
