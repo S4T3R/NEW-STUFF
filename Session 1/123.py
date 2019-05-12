c29a9831be4c1d0258eb8f70aefc53f61a745060
Matin <martin188@gmail.com>
2019-05-12 16:08:55 +0700

    C4T 18

---
+++ Session 1/123.py
@@ -0,0 +1,10 @@
+# var = 1
+# var1 = 1.1
+# string = "welcome to the jungle bitch"
+# string1 = " aloha"
+# print(var + var1)
+# print(string + string1)
+
+so = int(input("pick a number: "))
+so1 = int(input("pick a number: "))
+print (so + so1)
